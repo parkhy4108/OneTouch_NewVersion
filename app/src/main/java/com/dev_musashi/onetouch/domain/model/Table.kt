b249af1db5f58c6908187d2f370c87116e25f710
parkhy4108 <parkhy4108@gmail.com>
2023-07-20 14:17:01 +0900

    DOCS : 빈 줄 수정

--- app/src/main/java/com/dev_musashi/onetouch/domain/model/Table.kt
+++ app/src/main/java/com/dev_musashi/onetouch/domain/model/Table.kt
@@ -1,13 +1,10 @@
 package com.dev_musashi.onetouch.domain.model
 
-import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "table")
 data class Table(
-
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val timestamp: Long,
