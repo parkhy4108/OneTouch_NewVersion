52a073633412c74b2934c760d6329d42cda526fd
parkhy4108 <parkhy4108@gmail.com>
2023-07-20 14:25:57 +0900

    Update : Delete no use import

--- app/src/main/java/com/dev_musashi/onetouch/domain/usecase/DeleteTable.kt
+++ app/src/main/java/com/dev_musashi/onetouch/domain/usecase/DeleteTable.kt
@@ -1,6 +1,5 @@
 package com.dev_musashi.onetouch.domain.usecase
 
-import com.dev_musashi.onetouch.domain.model.Table
 import com.dev_musashi.onetouch.domain.repository.Repository
 import javax.inject.Inject
 
