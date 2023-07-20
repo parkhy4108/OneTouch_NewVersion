633f5165f1a1cb249dcf872328b122ec59caa7e0
parkhy4108 <parkhy4108@gmail.com>
2023-07-20 21:49:00 +0900

    Update : Add flash Click event

--- app/src/main/java/com/dev_musashi/onetouch/ui/camera/CAMERAUIEvent.kt
+++ app/src/main/java/com/dev_musashi/onetouch/ui/camera/CAMERAUIEvent.kt
@@ -6,5 +6,7 @@ import androidx.compose.ui.graphics.ImageBitmap
 sealed interface CAMERAUIEvent {
 
     object BoardClick : CAMERAUIEvent
+    object FlashClick : CAMERAUIEvent
     data class TakePictureButton(val captureImg: ImageBitmap, val pictureImg: Bitmap) : CAMERAUIEvent
+
 }
