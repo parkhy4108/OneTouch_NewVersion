b7dd07ec0d6a8e99e2f17d2ad41fd5215f2a4291
parkhy4108 <parkhy4108@gmail.com>
2023-07-24 22:24:13 +0900

    Refactor : fullHideStatusBar -> setFullScreen
    Update feature about setFullScreen

--- app/src/main/java/com/dev_musashi/onetouch/MainActivity.kt
+++ app/src/main/java/com/dev_musashi/onetouch/MainActivity.kt
@@ -6,54 +6,71 @@ import android.os.Bundle
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowInsetsController
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.core.view.WindowCompat
 import com.dev_musashi.onetouch.presentation.common.snackBar.SnackBarManager
 import dagger.hilt.android.AndroidEntryPoint
 import com.dev_musashi.onetouch.R.string as AppText
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val buildVersion = Build.VERSION.SDK_INT
+    private val permissions =  arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val buildVersion = Build.VERSION.SDK_INT
-        requestPermissionLauncher.launch(arrayOf(
-            Manifest.permission.CAMERA,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ))
+
+        requestPermissionLauncher.launch(permissions)
 
         setContent { MainContents() }
 
-        fullHideStatusBar(buildVersion)
+        setFullScreen(buildVersion)
     }
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissionList ->
-        permissionList.forEach { isGranted ->
-            if(!isGranted.value) {
+        permissionList.forEach { permission ->
+            if (permission.key == Manifest.permission.CAMERA && !permission.value) {
                 SnackBarManager.showMessage(AppText.CameraPermissionDenied)
+            }
+            if (permission.key == Manifest.permission.READ_EXTERNAL_STORAGE && !permission.value) {
+                SnackBarManager.showMessage(AppText.StoragePermissionDenied)
             }
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.S)
-    private fun fullHideStatusBar(buildVersion: Int){
-        if(buildVersion >= Build.VERSION_CODES.M && buildVersion < Build.VERSION_CODES.R){
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
+
+    private fun setFullScreen(buildVersion: Int) {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
+        )
+        @RequiresApi(Build.VERSION_CODES.S)
+        if (buildVersion < Build.VERSION_CODES.R) {
             actionBar?.hide()
-        } else if(buildVersion >= Build.VERSION_CODES.R){
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    )
+        } else {
             window.setDecorFitsSystemWindows(false)
-            WindowCompat.setDecorFitsSystemWindows(window, false)
             val windowController = window.insetsController
             if (windowController != null) {
-                windowController.hide(WindowInsets.Type.systemBars())
-                windowController.systemBarsBehavior = if(buildVersion >= Build.VERSION_CODES.S) WindowInsetsController.BEHAVIOR_DEFAULT else WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                windowController.hide(WindowInsets.Type.systemBars() or WindowInsets.Type.navigationBars())
+                windowController.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
         }
     }
