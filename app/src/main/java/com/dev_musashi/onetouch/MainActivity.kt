1653366bb640e30c733ad3d741c8d19fee78052b
parkhy4108 <parkhy4108@gmail.com>
2023-07-26 01:35:13 +0900

    Update : Delete FullScreen function

--- app/src/main/java/com/dev_musashi/onetouch/MainActivity.kt
+++ app/src/main/java/com/dev_musashi/onetouch/MainActivity.kt
@@ -3,10 +3,6 @@ package com.dev_musashi.onetouch
 import android.Manifest
 import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -17,7 +13,6 @@ import com.dev_musashi.onetouch.R.string as AppText
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val buildVersion = Build.VERSION.SDK_INT
     private val permissions =  arrayOf(
         Manifest.permission.CAMERA,
         Manifest.permission.READ_EXTERNAL_STORAGE
@@ -25,12 +20,8 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         requestPermissionLauncher.launch(permissions)
-
         setContent { MainContents() }
-
-        setFullScreen(buildVersion)
     }
 
     private val requestPermissionLauncher = registerForActivityResult(
@@ -42,35 +33,6 @@ class MainActivity : ComponentActivity() {
             }
             if (permission.key == Manifest.permission.READ_EXTERNAL_STORAGE && !permission.value) {
                 SnackBarManager.showMessage(AppText.StoragePermissionDenied)
-            }
-        }
-    }
-
-
-
-
-    private fun setFullScreen(buildVersion: Int) {
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
-        )
-        @RequiresApi(Build.VERSION_CODES.S)
-        if (buildVersion < Build.VERSION_CODES.R) {
-            actionBar?.hide()
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = (
-                    View.SYSTEM_UI_FLAG_IMMERSIVE
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    )
-        } else {
-            window.setDecorFitsSystemWindows(false)
-            val windowController = window.insetsController
-            if (windowController != null) {
-                windowController.hide(WindowInsets.Type.systemBars() or WindowInsets.Type.navigationBars())
-                windowController.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
         }
     }
