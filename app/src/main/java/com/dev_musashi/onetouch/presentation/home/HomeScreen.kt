775ea9f8719c89793393e2804f4868965313e78b
parkhy4108 <parkhy4108@gmail.com>
2023-07-25 23:49:57 +0900

    Style : UI Update
    1. DarkTheme & WhiteTheme Update
    2. CameraButton -> Floating Button

--- app/src/main/java/com/dev_musashi/onetouch/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/dev_musashi/onetouch/presentation/home/HomeScreen.kt
@@ -4,10 +4,14 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.provider.MediaStore
-import androidx.compose.foundation.background
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,22 +19,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
@@ -47,6 +52,10 @@ import com.dev_musashi.onetouch.presentation.home.composable.AddDialog
 import com.dev_musashi.onetouch.presentation.home.composable.ButtonList
 import com.dev_musashi.onetouch.presentation.home.composable.DeleteDialog
 import com.dev_musashi.onetouch.presentation.home.composable.Table
+import com.dev_musashi.onetouch.presentation.theme.Purple40
+import com.dev_musashi.onetouch.presentation.theme.Purple80
+import com.dev_musashi.onetouch.presentation.theme.Yellow40
+import com.dev_musashi.onetouch.presentation.theme.Yellow80
 import com.dev_musashi.onetouch.R.drawable as AppImg
 import com.dev_musashi.onetouch.R.string as AppText
 
@@ -58,111 +67,147 @@ fun HomeScreen(
     val state by homeViewModel.state.collectAsState()
     val onEvent = homeViewModel::onEvent
     val focusManager = LocalFocusManager.current
-    val focusRequester by remember { mutableStateOf(FocusRequester()) }
     val lazyListState = rememberLazyListState()
     val context = LocalContext.current
     val intent =
         Intent(Intent.ACTION_VIEW).apply { this.type = MediaStore.Images.Media.CONTENT_TYPE }
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        item {
-            Column(
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    if (ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.CAMERA
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        onEvent(HOMEUIEvent.OpenCamera(openScreen))
+                    } else {
+                        SnackBarManager.showMessage(AppText.CameraPermissionDenied)
+                    }
+                },
+                containerColor = if (isSystemInDarkTheme()) Purple40 else Purple80,
+                shape = CircleShape,
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = AppImg.ic_camera),
+                    contentDescription = stringResource(id = AppText.Camera)
+                )
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .addFocusCleaner(focusManager)
+        ) {
+            //One Touch Title & Gallery Button
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .addFocusCleaner(focusManager)
+                    .padding(15.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
+                Text(
+                    text = stringResource(id = AppText.OneTouch),
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold
+                )
+                Icon(
+                    modifier = Modifier.clickable {
+                        if (ContextCompat.checkSelfPermission(
+                                context,
+                                Manifest.permission.CAMERA
+                            ) == PackageManager.PERMISSION_GRANTED
+                        ) {
+                            context.startActivity(intent)
+                        } else {
+                            SnackBarManager.showMessage(AppText.StoragePermissionDenied)
+                        }
+                    },
+                    painter = painterResource(id = AppImg.ic_photo),
+                    contentDescription = "gallery"
+                )
+            }
 
-                //One Touch Title & Gallery Button
-                Row(
-                    modifier = Modifier
+            //History List
+            val cardBGColor = if (isSystemInDarkTheme()) Purple40 else Purple80
+            Text(
+                modifier = Modifier
+                    .padding(20.dp, 0.dp),
+                text = stringResource(id = AppText.History),
+                style = MaterialTheme.typography.labelLarge
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(20.dp),
+                colors = CardDefaults.cardColors(cardBGColor),
+                elevation = CardDefaults.elevatedCardElevation(3.dp)
+            ) {
+                val dividerColor = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray
+                if (state.history.isNotEmpty()) {
+                    LazyColumn(modifier = Modifier
                         .fillMaxWidth()
-                        .height(40.dp)
-                        .padding(10.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        text = stringResource(id = AppText.OneTouch),
-                        color = Color.Black,
-                        fontSize = 12.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Icon(
-                        modifier = Modifier.clickable {
-                            if (ContextCompat.checkSelfPermission(
-                                    context,
-                                    Manifest.permission.CAMERA
-                                ) == PackageManager.PERMISSION_GRANTED
-                            ) {
-
-
-                                context.startActivity(intent)
-                            } else {
-                                SnackBarManager.showMessage(AppText.StoragePermissionDenied)
-                            }
-                        },
-                        painter = painterResource(id = AppImg.ic_photo),
-                        contentDescription = "gallery"
-                    )
-                }
-
-                //History List
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp)
-                        .background(Color.Black)
-                ) {
-                    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                        .padding(10.dp)) {
                         items(state.history) { history ->
                             Text(
                                 modifier = Modifier.fillMaxWidth(),
                                 text = history.str,
-                                fontSize = 13.sp,
-                                color = Color.White
+                                fontSize = 13.sp
                             )
-                            Divider(color = Color.LightGray, thickness = 1.dp)
+                            Spacer(modifier = Modifier.height(5.dp))
+                            Divider(color = dividerColor,thickness = 1.dp)
                         }
                     }
                 }
 
-                //보드판 서식 입력 Text & 저장 버튼  & 추가 버튼
+            }
+
+            //보드판 서식 입력 Text & 저장 버튼  & 추가 버튼
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = stringResource(id = AppText.BoardTitle),
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold
+                )
                 Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(40.dp)
-                        .padding(10.dp),
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
+                    horizontalArrangement = Arrangement.spacedBy(15.dp)
                 ) {
-                    Text(
-                        text = stringResource(id = AppText.BoardTitle),
-                        fontSize = 12.sp,
-                        fontWeight = FontWeight.Bold
+                    Icon(
+                        modifier = Modifier.clickable {
+                            focusManager.clearFocus()
+                            onEvent(HOMEUIEvent.SaveTable)
+                        },
+                        painter = painterResource(id = AppImg.ic_save),
+                        contentDescription = null
                     )
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(15.dp)
-                    ) {
-                        Icon(
-                            modifier = Modifier.clickable {
-                                focusManager.clearFocus()
-                                onEvent(HOMEUIEvent.SaveTable)
-                            },
-                            painter = painterResource(id = AppImg.ic_save),
-                            contentDescription = null
-                        )
-                        Icon(
-                            modifier = Modifier.clickable {
-                                onEvent(HOMEUIEvent.ShowAddDialog)
-                            },
-                            painter = painterResource(id = AppImg.ic_add),
-                            contentDescription = "add"
-                        )
-                    }
+                    Icon(
+                        modifier = Modifier.clickable {
+                            onEvent(HOMEUIEvent.ShowAddDialog)
+                        },
+                        painter = painterResource(id = AppImg.ic_add),
+                        contentDescription = "add"
+                    )
                 }
-                Divider(color = Color.LightGray)
-                Spacer(modifier = Modifier.height(3.dp))
-                //버튼 리스트
+            }
+
+            //버튼 리스트
+            AnimatedVisibility(
+                visible = state.titleBtnList.isNotEmpty(),
+                enter = fadeIn() + expandVertically(),
+                exit = fadeOut() + shrinkVertically()
+            ) {
                 ButtonList(
                     list = state.titleBtnList,
                     lazyListState = lazyListState,
@@ -170,64 +215,26 @@ fun HomeScreen(
                     focusManager = focusManager,
                     onEvent = onEvent
                 )
-                Spacer(modifier = Modifier.height(3.dp))
-
-                Divider(color = Color.LightGray)
-
-                //테이블 content 영역
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(220.dp)
-                ) {
-                    if (state.currentBtn != null) {
-                        Table(
-                            state = state,
-                            onEvent = onEvent,
-                            focusRequester = focusRequester
-                        )
-                    }
-                }
-
-                //촬영 버튼 영역
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp)
-                        .padding(10.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Button(
-                        modifier = Modifier.width(200.dp),
-                        onClick = {
-                            if (ContextCompat.checkSelfPermission(
-                                    context,
-                                    Manifest.permission.CAMERA
-                                ) == PackageManager.PERMISSION_GRANTED
-                            ) {
-                                onEvent(HOMEUIEvent.OpenCamera(openScreen))
-                            } else {
-                                SnackBarManager.showMessage(AppText.CameraPermissionDenied)
-                            }
-
-                        }
-                    ) {
-                        Text(text = "촬영", fontSize = 10.sp)
-                    }
-                }
-                Divider(color = Color.LightGray)
             }
-        }
-        item {
-            Box(
+
+            //테이블 content 영역
+            val tableCardBGColor = if (isSystemInDarkTheme()) Yellow40 else Yellow80
+            Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(600.dp)
+                    .height(240.dp)
+                    .padding(20.dp),
+                elevation = CardDefaults.elevatedCardElevation(3.dp),
+                colors = CardDefaults.cardColors(tableCardBGColor)
             ) {
-
+                if(state.currentBtn != null) {
+                    Table(
+                        modifier = Modifier.weight(1f),
+                        state = state,
+                        onEvent = onEvent
+                    )
+                }
             }
-            Divider(color = Color.LightGray)
         }
     }
 
@@ -238,4 +245,5 @@ fun HomeScreen(
     if (state.isDeletingTable) {
         DeleteDialog(onEvent = onEvent)
     }
+
 }
