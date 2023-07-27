d43f35c0c959202f1be85e56eea98f599331aa9e
parkhy4108 <parkhy4108@gmail.com>
2023-07-28 02:28:46 +0900

    Style : change Some styles
    1. ButtonList.kt padding 20.dp -> 5.dp
    2. change folder name data util -> bitmap
    3. add table argument focusManager
    4. add keyboardOption in tableRow textField

--- app/src/main/java/com/dev_musashi/onetouch/presentation/home/composable/Table.kt
+++ app/src/main/java/com/dev_musashi/onetouch/presentation/home/composable/Table.kt
@@ -1,30 +1,14 @@
 package com.dev_musashi.onetouch.presentation.home.composable
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.dev_musashi.onetouch.R
 import com.dev_musashi.onetouch.presentation.home.HOMEUIEvent
 import com.dev_musashi.onetouch.presentation.home.HomeState
 import com.dev_musashi.onetouch.R.string as AppText
@@ -32,6 +16,7 @@ import com.dev_musashi.onetouch.R.string as AppText
 @Composable
 fun Table(
     modifier : Modifier,
+    focusManager: FocusManager,
     state: HomeState,
     onEvent: (HOMEUIEvent) -> Unit
 ) {
@@ -42,6 +27,7 @@ fun Table(
     ) {
         TableRow(
             modifier = Modifier.weight(1f),
+            focusManager = focusManager,
             text = stringResource(id = AppText.Name),
             value = state.name,
             onEvent = { onEvent(HOMEUIEvent.SetName(it)) },
@@ -49,6 +35,7 @@ fun Table(
         Divider(modifier = Modifier.fillMaxWidth())
         TableRow(
             modifier = Modifier.weight(1f),
+            focusManager = focusManager,
             text = stringResource(id = AppText.Species),
             value = state.species,
             onEvent = { onEvent(HOMEUIEvent.SetSpecies(it)) },
@@ -56,6 +43,7 @@ fun Table(
         Divider(modifier = Modifier.fillMaxWidth())
         TableRow(
             modifier = Modifier.weight(1f),
+            focusManager = focusManager,
             text = stringResource(id = AppText.Location),
             value = state.location,
             onEvent = { onEvent(HOMEUIEvent.SetLocation(it)) },
@@ -63,6 +51,7 @@ fun Table(
         Divider(modifier = Modifier.fillMaxWidth())
         TableRow(
             modifier = Modifier.weight(1f),
+            focusManager = focusManager,
             text = stringResource(id = AppText.Date),
             value = state.date,
             onEvent = { onEvent(HOMEUIEvent.SetDate(it)) },
@@ -70,51 +59,11 @@ fun Table(
         Divider(modifier = Modifier.fillMaxWidth())
         TableRow(
             modifier = Modifier.weight(1f),
+            focusManager = focusManager,
             text = stringResource(id = AppText.Note),
             value = state.note,
             onEvent = { onEvent(HOMEUIEvent.SetNote(it)) },
         )
     }
 
-}
-
-@Preview(showBackground = true)
-@Composable
-fun RowPreview() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(40.dp),
-        horizontalArrangement = Arrangement.spacedBy(6.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = "text",
-            modifier = Modifier
-                .weight(0.2f)
-                .height(40.dp)
-                .wrapContentHeight(align = Alignment.CenterVertically),
-            color = Color.Black,
-            textAlign = TextAlign.Center
-        )
-        Spacer(modifier = Modifier)
-        BasicTextField(
-            modifier = Modifier
-                .weight(0.7f)
-                .height(40.dp)
-                .wrapContentHeight(Alignment.CenterVertically),
-            value = "value",
-            onValueChange = { },
-            textStyle = TextStyle.Default.copy(
-                fontSize = 12.sp, color = Color.Black
-            )
-        )
-        Icon(
-            modifier = Modifier
-                .weight(0.1f)
-                .clickable { },
-            painter = painterResource(id = R.drawable.ic_cancel),
-            contentDescription = null
-        )
-    }
 }
--- app/src/main/java/com/dev_musashi/onetouch/presentation/home/composable/TableRow.kt
+++ app/src/main/java/com/dev_musashi/onetouch/presentation/home/composable/TableRow.kt
@@ -1,5 +1,6 @@
 package com.dev_musashi.onetouch.presentation.home.composable
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
@@ -9,22 +10,29 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.dev_musashi.onetouch.R
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TableRow(
     modifier: Modifier,
+    focusManager: FocusManager,
     text: String,
     value: String,
     onEvent: (String) -> Unit
@@ -59,7 +67,12 @@ fun TableRow(
                 ) {
                     innerTextField()
                 }
-            }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
         )
 
         Icon(
@@ -68,7 +81,7 @@ fun TableRow(
                 .clickable { onEvent("") },
             painter = painterResource(id = R.drawable.ic_cancel),
             contentDescription = null,
-            tint = if(isSystemInDarkTheme()) Color.DarkGray else Color.LightGray
+            tint = if (isSystemInDarkTheme()) Color.DarkGray else Color.LightGray
         )
     }
 }
